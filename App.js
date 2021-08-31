c51d564a944aad5d0c1e57fc85645f466941511d
Patrick <ptrckxforcadela@gmail.com>
2021-08-31 16:29:20 +0800

    Passing value using props

--- App.js
+++ App.js
@@ -10,7 +10,8 @@ export default function App() {
         <Text style={styles.sectionTitle}>Today's tasks</Text>
         <View style={styles.items}>
           {/* This is where the tasks will go */}
-          <Task />
+          <Task text={"Task 1"} />
+          <Task text={"Task 2"} />
         </View>
       </View>
     </View>
--- components/Task.js
+++ components/Task.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
-export default function Task() {
+export default function Task({ text }) {
     return (
         <View>
-            <Text>This is a task</Text>
+            <Text>{text}</Text>
         </View>
     )
 }
